41291f56726fd105ee62c5982cbd1084f53f28e4
Cursed Code <lekashala3@gmail.com>
2025-04-09 02:58:38 +0200

    remove psycopg and see if works

--- api/db_manager.py
+++ api/db_manager.py
@@ -1,121 +1,121 @@
-import psycopg2
-
-
-HOST = '127.0.0.1'
-DATABASE = 'postgres'
-USER = 'postgres'
-PASSWORD = 'toor'
-PORT = 5432
-
-FREE_DEFAULT_USES = 3
-PREMIUM_DEFAULT_USES = 10
-PREMIUM_DEFAULT_USES_YEARLY = 100
-
-
-def cur_wrapper(func):
-    def wrapper(*args, **kwargs):
-        con = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=PORT)
-        cur = con.cursor()
-        kwargs['cur'] = cur
-        result = func(*args, **kwargs)
-        con.commit()
-        cur.close()
-        con.close()
-        return result
-    return wrapper
-
-
-@cur_wrapper
-def create_users_table(cur=None):
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id SERIAL PRIMARY KEY,
-        client_id TEXT NOT NULL,
-        birthday DATE NOT NULL,
-        is_male BOOLEAN NOT NULL,
-        email TEXT DEFAULT '',
-        is_premium BOOLEAN DEFAULT false,
-        premium_purchase_time TIMESTAMP NULL,
-        created_at TIMESTAMPTZ DEFAULT now()
-    );
-    ''')
-
-
-@cur_wrapper
-def create_images_table(cur=None):
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS images (
-        id SERIAL PRIMARY KEY,
-        user_id INT NOT NULL,
-        score FLOAT DEFAULT 1200,
-        is_male BOOLEAN NOT NULL,
-        url TEXT NOT NULL,
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        created_at TIMESTAMPTZ DEFAULT now()
-    );
-    ''')
-
-
-@cur_wrapper
-def create_ratings_table(cur=None):
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS ratings (
-        id SERIAL PRIMARY KEY,
-        image1_id INT NOT NULL,
-        image2_id INT NOT NULL,
-        is_image1_voted BOOLEAN NOT NULL,
-        created_at TIMESTAMPTZ DEFAULT now()
-        
-    );
-    ''')
-
-
-create_users_table()
-create_images_table()
-create_ratings_table()
-
-
-@cur_wrapper
-def get_user_from_client_id(client_id: str, birthday: str, is_male: bool, cur=None):
-    cur.execute(f'''SELECT * FROM users WHERE client_id = '{client_id}';''')
-    user = cur.fetchone()
-
-    # If user doesn't exist, create one
-    if not user:
-        cur.execute(f'''
-            INSERT INTO users (client_id, birthday, is_male) 
-            VALUES ('{client_id}', '{birthday}', {is_male})
-            RETURNING *;
-        ''')
-        user = cur.fetchone()
-        return user
-    ###################################
-
-    # (id, client_id, birthday, is_male, email, is_premium, premium_purchase_date, created_at)
-    return user
-
-
-@cur_wrapper
-def create_new_image_record(user_id: int, is_male: bool, img_format: str = '.jpg', cur=None) -> tuple:
-    cur.execute(f'''
-    INSERT INTO images (is_male, url, user_id) 
-    VALUES (
-    {is_male},
-    (concat('user_images/{user_id}/', currval('images_id_seq')::text, '{img_format}')),
-    {user_id}
-    )
-    RETURNING *;
-    ''')
-    image = cur.fetchone()
-    # (id, user_id, score, is_male, is_male, url, created_at)
-    return image
-
-
-@cur_wrapper
-def buy_monthly_premium_with_client_id(client_id, cur=None):
-    cur.execute(f'''UPDATE users SET is_premium = TRUE, premium_purchase_time = NOW() WHERE client_id = '{client_id}';''')
-
-
-@cur_wrapper
-def cancel_premium_with_client_id(client_id, cur=None):
-    cur.execute(f'''UPDATE users SET is_premium = FALSE, premium_purchase_time = NULL WHERE client_id = '{client_id}';''')
+# import psycopg2
+#
+#
+# HOST = '127.0.0.1'
+# DATABASE = 'postgres'
+# USER = 'postgres'
+# PASSWORD = 'toor'
+# PORT = 5432
+#
+# FREE_DEFAULT_USES = 3
+# PREMIUM_DEFAULT_USES = 10
+# PREMIUM_DEFAULT_USES_YEARLY = 100
+#
+#
+# def cur_wrapper(func):
+#     def wrapper(*args, **kwargs):
+#         con = psycopg2.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD, port=PORT)
+#         cur = con.cursor()
+#         kwargs['cur'] = cur
+#         result = func(*args, **kwargs)
+#         con.commit()
+#         cur.close()
+#         con.close()
+#         return result
+#     return wrapper
+#
+#
+# @cur_wrapper
+# def create_users_table(cur=None):
+#     cur.execute('''
+#     CREATE TABLE IF NOT EXISTS users (
+#         id SERIAL PRIMARY KEY,
+#         client_id TEXT NOT NULL,
+#         birthday DATE NOT NULL,
+#         is_male BOOLEAN NOT NULL,
+#         email TEXT DEFAULT '',
+#         is_premium BOOLEAN DEFAULT false,
+#         premium_purchase_time TIMESTAMP NULL,
+#         created_at TIMESTAMPTZ DEFAULT now()
+#     );
+#     ''')
+#
+#
+# @cur_wrapper
+# def create_images_table(cur=None):
+#     cur.execute('''
+#     CREATE TABLE IF NOT EXISTS images (
+#         id SERIAL PRIMARY KEY,
+#         user_id INT NOT NULL,
+#         score FLOAT DEFAULT 1200,
+#         is_male BOOLEAN NOT NULL,
+#         url TEXT NOT NULL,
+#         FOREIGN KEY (user_id) REFERENCES users(id),
+#         created_at TIMESTAMPTZ DEFAULT now()
+#     );
+#     ''')
+#
+#
+# @cur_wrapper
+# def create_ratings_table(cur=None):
+#     cur.execute('''
+#     CREATE TABLE IF NOT EXISTS ratings (
+#         id SERIAL PRIMARY KEY,
+#         image1_id INT NOT NULL,
+#         image2_id INT NOT NULL,
+#         is_image1_voted BOOLEAN NOT NULL,
+#         created_at TIMESTAMPTZ DEFAULT now()
+#
+#     );
+#     ''')
+#
+#
+# create_users_table()
+# create_images_table()
+# create_ratings_table()
+#
+#
+# @cur_wrapper
+# def get_user_from_client_id(client_id: str, birthday: str, is_male: bool, cur=None):
+#     cur.execute(f'''SELECT * FROM users WHERE client_id = '{client_id}';''')
+#     user = cur.fetchone()
+#
+#     # If user doesn't exist, create one
+#     if not user:
+#         cur.execute(f'''
+#             INSERT INTO users (client_id, birthday, is_male)
+#             VALUES ('{client_id}', '{birthday}', {is_male})
+#             RETURNING *;
+#         ''')
+#         user = cur.fetchone()
+#         return user
+#     ###################################
+#
+#     # (id, client_id, birthday, is_male, email, is_premium, premium_purchase_date, created_at)
+#     return user
+#
+#
+# @cur_wrapper
+# def create_new_image_record(user_id: int, is_male: bool, img_format: str = '.jpg', cur=None) -> tuple:
+#     cur.execute(f'''
+#     INSERT INTO images (is_male, url, user_id)
+#     VALUES (
+#     {is_male},
+#     (concat('user_images/{user_id}/', currval('images_id_seq')::text, '{img_format}')),
+#     {user_id}
+#     )
+#     RETURNING *;
+#     ''')
+#     image = cur.fetchone()
+#     # (id, user_id, score, is_male, is_male, url, created_at)
+#     return image
+#
+#
+# @cur_wrapper
+# def buy_monthly_premium_with_client_id(client_id, cur=None):
+#     cur.execute(f'''UPDATE users SET is_premium = TRUE, premium_purchase_time = NOW() WHERE client_id = '{client_id}';''')
+#
+#
+# @cur_wrapper
+# def cancel_premium_with_client_id(client_id, cur=None):
+#     cur.execute(f'''UPDATE users SET is_premium = FALSE, premium_purchase_time = NULL WHERE client_id = '{client_id}';''')
--- api/index.py
+++ api/index.py
@@ -5,7 +5,6 @@ import cv2 as cv
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import random
-from api import db_manager
 from utils import girl_images
 
 
@@ -25,67 +24,74 @@ def get_new_image_path(user_id, file_extension):
     new_image_path = os.path.join(new_user_id_dir, f'{dir_count}{file_extension}')
     return new_image_path
 
+#
+# from api import db_manager
+#
+#
+# @app.route('/buy-monthly-premium', methods=['GET'])
+# def buy_monthly_premium():
+#     try:
+#         if random.randint(0, 1) == 1:
+#             return {'purchase': False}
+#
+#         client_id = request.args.get('id')
+#         db_manager.buy_monthly_premium_with_client_id(client_id=client_id)
+#         return {'purchase': True}
+#     except ...:
+#         return {'purchase': False}
+#
+#
+# @app.route('/cancel-premium', methods=['GET'])
+# def cancel_premium():
+#     try:
+#         if random.randint(0, 1) == 1:
+#             return {'cancel': False}
+#
+#         client_id = request.args.get('id')
+#         db_manager.cancel_premium_with_client_id(client_id=client_id)
+#         return {'cancel': True}
+#     except ...:
+#         return {'cancel': False}
+#
+#
+# @app.route('/upload-image-and-rate', methods=['POST'])
+# def upload_image_and_rate():
+#     try:
+#         file = None
+#         client_id = request.args.get('id')
+#         is_male = bool(request.args.get('is_male'))
+#         birthday = request.args.get('birthday')
+#
+#         if 'image' in request.files:
+#             file = request.files['image']
+#
+#         if not file:
+#             print('File is not here', file)
+#             return {'images_to_rate': []}, 400
+#
+#         file_name = file.filename
+#         if not file_name:
+#             return jsonify({'error': 'No selected file'}), 400
+#
+#         user_id = db_manager.get_user_from_client_id(client_id=client_id, birthday=birthday, is_male=is_male)[0]
+#
+#         file_bytes = np.fromfile(file, dtype=np.uint8)
+#         img = cv.imdecode(file_bytes, cv.IMREAD_COLOR)
+#
+#         file_extension = f'.{file_name.split(".")[-1]}'
+#         session_img_path = get_new_image_path(user_id, file_extension)
+#         cv.imwrite(session_img_path, img)
+#         db_manager.create_new_image_record(user_id=user_id, is_male=is_male, img_format=file_extension)
+#
+#         return {'images_to_rate': girl_images}
+#     except ...:
+#         print('Error happend')
+#         return {'images_to_rate': []}, 404
 
-@app.route('/buy-monthly-premium', methods=['GET'])
-def buy_monthly_premium():
-    try:
-        if random.randint(0, 1) == 1:
-            return {'purchase': False}
 
-        client_id = request.args.get('id')
-        db_manager.buy_monthly_premium_with_client_id(client_id=client_id)
-        return {'purchase': True}
-    except ...:
-        return {'purchase': False}
-
-
-@app.route('/cancel-premium', methods=['GET'])
-def cancel_premium():
-    try:
-        if random.randint(0, 1) == 1:
-            return {'cancel': False}
-
-        client_id = request.args.get('id')
-        db_manager.cancel_premium_with_client_id(client_id=client_id)
-        return {'cancel': True}
-    except ...:
-        return {'cancel': False}
-
-
-@app.route('/upload-image-and-rate', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_image_and_rate():
-    try:
-        file = None
-        client_id = request.args.get('id')
-        is_male = bool(request.args.get('is_male'))
-        birthday = request.args.get('birthday')
-
-        if 'image' in request.files:
-            file = request.files['image']
-
-        if not file:
-            print('File is not here', file)
-            return {'images_to_rate': []}, 400
-
-        file_name = file.filename
-        if not file_name:
-            return jsonify({'error': 'No selected file'}), 400
-
-        user_id = db_manager.get_user_from_client_id(client_id=client_id, birthday=birthday, is_male=is_male)[0]
-
-        file_bytes = np.fromfile(file, dtype=np.uint8)
-        img = cv.imdecode(file_bytes, cv.IMREAD_COLOR)
-
-        file_extension = f'.{file_name.split(".")[-1]}'
-        session_img_path = get_new_image_path(user_id, file_extension)
-        cv.imwrite(session_img_path, img)
-        db_manager.create_new_image_record(user_id=user_id, is_male=is_male, img_format=file_extension)
-
-        return {'images_to_rate': girl_images}
-    except ...:
-        print('Error happend')
-        return {'images_to_rate': []}, 404
-
+    return 'hellooooooo'
 
 if __name__ == '__main__':
     app.run()
